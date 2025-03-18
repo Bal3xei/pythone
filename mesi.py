62d00b2f935ddf5774cf58c69cee8571ec8394de
Alex <brasovianu.alex2@gmail.com>
2025-03-18 15:54:55 +0100

    aggiornato

--- mesi.py
+++ mesi.py
@@ -1,5 +1,2 @@
 mesi = [("Gennaio"), ("Febbraio"),("Marzo"),("Aprile"),("Maggio"),("Giugno"),("Luglio"),("Agosto"),("Settembre"),("Ottobre"),("Novembre"),("Dicembre")]
-# num = int(input())
-# print(mesi[(num - 1)])
-
 print(mesi[(int(input())-1)])
