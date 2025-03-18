b92bace4f8d194bd62eed524d4c9a4d5569e5336
Alex <brasovianu.alex2@gmail.com>
2025-03-18 14:00:38 +0100

    collapsed_version

--- somma.py
+++ somma.py
@@ -1,9 +1,9 @@
-#print(int(input()) + int(input()))
+print(int(input("passami il primo numero")) + int(input("passami il secondo numero")))
 
-valore_1= input("passami il primo numero:")
-valore_2= input("passami il secondo numero:")
+# valore_1= input("passami il primo numero:")
+# valore_2= input("passami il secondo numero:")
 
-v1= int(valore_1)
-v2=int(valore_2)
+# v1= int(valore_1)
+# v2=int(valore_2)
 
-print(v1 + v2)
+# print("La somma dei numeri è",v1 + v2)
