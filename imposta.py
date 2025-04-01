34e7a8e52d731d502a21da1d74732b5d4331c47f
Alex <brasovianu.alex2@gmail.com>
2025-04-01 13:04:00 +0200

    imposta

---
+++ imposta.py
@@ -0,0 +1,17 @@
+aliquotaA= 0.23
+aliquotaB = 0.35
+aliquotaC = 0.43
+scaglione1= 28000
+scaglione2= 50000
+
+reddito =int(input("Insersici il reddito"))
+
+if reddito<=scaglione1:
+    tassazione=reddito*aliquotaA
+else:    
+    if reddito > scaglione1 and reddito <= scaglione2:
+        tassazione=(scaglione1*aliquotaA)+((reddito-scaglione1)*aliquotaB)
+    else: 
+        tassazione=(scaglione1*aliquotaA)+((reddito-scaglione1)*aliquotaB)+((reddito-scaglione2)*aliquotaC)
+            
+print(tassazione)
