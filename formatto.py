d2dd3ca7e7f948b9fa561a30f08eda564c466557
Alex <brasovianu.alex2@gmail.com>
2025-04-09 11:53:11 +0200

    compattazione

--- formatto.py
+++ formatto.py
@@ -1,3 +1,12 @@
 
-for x in (range(1,11)):
-    print(f"{(1/x):12.8f}")
+
+lista = [print(f"{1/x:.8f}") or x for x in range(1, 11)] 
+#assegna numeri interi e stampa il reciproco formattato
+
+# lista= [ 1/x for x in (range(1,11)) % "%.2f" ]
+# print(lista)
+
+# lista = [f"{1/x:.8f}" for x in range(1, 11)]
+# print(lista,sep="\n")
+
+
