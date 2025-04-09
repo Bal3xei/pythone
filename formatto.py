ac6cb1bf6b5ae802b652941ff7f339a3b61252a2
Alex <brasovianu.alex2@gmail.com>
2025-04-09 11:58:04 +0200

    aggiornato

--- formatto.py
+++ formatto.py
@@ -1,6 +1,6 @@
 
 
-lista = [print(f"{1/x:.8f}") or x for x in range(1, 11)] 
+lista = [print(f"{1/x:.8f}") or 1/x for x in range(1, 11)] 
 #assegna numeri interi e stampa il reciproco formattato
 
 # lista= [ 1/x for x in (range(1,11)) % "%.2f" ]
